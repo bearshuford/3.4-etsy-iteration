33b034629be972aede26e8c5ce880f47101ab268
Gregory Shuford <bearshuford@me.com>
2016-09-29 14:34:54 -0400

    completed question 2

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ var sum = prices.reduce(function(total,current) {
 
 var avg = (sum/items.length).toFixed(2);  // calculate average to hundredths
 console.log('The average price is $', avg);
+
+/////////
+// TWO
+// array of items that cost b/w $14.00 & $18.00
+////////////////
+var twoArray = items.filter(function(obj) {
+   return (obj.price > 14 && obj.price < 18)
+}); // filter by price criteria
+console.log("Items that cost between $14.00 USD and $18.00 USD:", twoArray);
